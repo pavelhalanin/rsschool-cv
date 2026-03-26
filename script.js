1b04855b4a1481417b93c382a12b4620f3c6aa7b
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-03-27 00:02:22 +0300

    feat: bootstrap removed, content updated

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('#offcanvasNavbar .nav-link').forEach(link => {
-    link.addEventListener('click', () => {
-        const offcanvas = bootstrap.Offcanvas.getInstance(document.getElementById('offcanvasNavbar'));
-        if (offcanvas) {
-            offcanvas.hide();
-        }
-    });
-});
